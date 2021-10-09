ede2a6cb51077a284e827a9c9a8c5c4b7f537866
blackhome7 <84621214+blackhome7@users.noreply.github.com>
2021-10-09 21:28:55 +0300

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,14 @@
+import random
+
+chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = input('Количество паролей?'+ "\n")
+length = input('Длина пароля?'+ "\n")
+number = int(number)
+length = int(length)
+for n in range(number):
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
+    print( "Ваш пароль: " + password)
+
+input('Нажмите Enter для выхода\n')
